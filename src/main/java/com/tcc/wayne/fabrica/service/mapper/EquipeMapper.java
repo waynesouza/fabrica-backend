f2a81e72a9ebba122fd36ed44c449984081bc0fb
Wayne <waynesouza07@gmail.com>
2022-01-05 19:56:01 -0300

    Ajuste na configuração do mapper

--- src/main/java/com/tcc/wayne/fabrica/service/mapper/EquipeMapper.java
+++ src/main/java/com/tcc/wayne/fabrica/service/mapper/EquipeMapper.java
@@ -3,9 +3,11 @@ package com.tcc.wayne.fabrica.service.mapper;
 import com.tcc.wayne.fabrica.domain.Equipe;
 import com.tcc.wayne.fabrica.service.dto.EquipeDTO;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface EquipeMapper {
 
